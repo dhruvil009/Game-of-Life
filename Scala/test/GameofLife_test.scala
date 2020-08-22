05863ed560c257433963ac2630afb80703b4e8b8
Dhruvil <dhruvilsshah@gmail.com>
2020-08-22 17:13:58 -0400

    Update Testing

--- Scala/test/GameofLife_test.scala
+++ Scala/test/GameofLife_test.scala
@@ -4,9 +4,13 @@ import org.scalatest._
 import scala.Array.ofDim
 
 class GameTest extends GameofLife {
-  val matrix = ofDim[Integer](5, 5)
+  it ("Null Population Never changes") {
+    var matrix = ofDim[Integer](5, 5)
+    Live(matrix, 5, 5, 2) should be (matrix)
+  }
 
   it ("Computes Matrix Correctly") {
+    var matrix = ofDim[Integer](5, 5)
     Live(matrix, 5, 5, 2) should be (matrix)
   }
 }
