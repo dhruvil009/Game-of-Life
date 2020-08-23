3c35bb7b77f1bbb65e822e95d2765d7afafbf4f0
sray93 <69826605+sray93@users.noreply.github.com>
2020-08-22 20:24:45 -0400

    Update GameofLife_test.scala
    Added test Big Matrix

--- Scala/src/test/scala/GameofLife_test.scala
+++ Scala/src/test/scala/GameofLife_test.scala
@@ -26,8 +26,61 @@ class GameTest  extends FunSpec with Matchers  {
   }
 
   it ("Computes Big Matrix Correctly") {
-//    ToDo: Sreemoyee
+    var matrix = ofDim[Integer](5, 5)
+    matrix(0)(0) = 1
+    matrix(0)(1) = 1
+	  matrix(0)(2) = 0
+    matrix(0)(3) = 1
+    matrix(0)(4) = 1
+    matrix(1)(0) = 0
+    matrix(1)(1) = 1
+    matrix(1)(2) = 0
+    matrix(1)(3) = 1
+    matrix(1)(4) = 1
+    matrix(2)(0) = 0
+    matrix(2)(1) = 0
+    matrix(2)(2) = 0
+    matrix(2)(3) = 1
+    matrix(2)(4) = 1
+    matrix(3)(0) = 1
+    matrix(3)(1) = 1
+    matrix(3)(2) = 0
+    matrix(3)(3) = 1
+    matrix(3)(4) = 0
+    matrix(4)(0) = 0
+    matrix(4)(1) = 0
+    matrix(4)(2) = 0
+    matrix(4)(3) = 0
+    matrix(4)(4) = 0
+	
+    var result = ofDim[Integer](5, 5)
 
-//    new src.main.scala.GameofLife().Live(matrix, 2, 2, 1) should be (result)
+	  result(0)(0) = 1
+    result(0)(1) = 1
+    result(0)(2) = 0
+    result(0)(3) = 1
+    result(0)(4) = 1
+    result(1)(0) = 1
+    result(1)(1) = 1
+    result(1)(2) = 0
+    result(1)(3) = 0
+    result(1)(4) = 0
+    result(2)(0) = 1
+    result(2)(1) = 1
+    result(2)(2) = 0
+    result(2)(3) = 0
+    result(2)(4) = 0
+    result(3)(0) = 0
+    result(3)(1) = 0
+    result(3)(2) = 1
+    result(3)(3) = 1
+    result(3)(4) = 1
+    result(4)(0) = 0
+    result(4)(1) = 0
+    result(4)(2) = 0
+    result(4)(3) = 0
+    result(4)(4) = 0
+
+//new src.main.scala.GameofLife().Live(matrix, 2, 2, 1) should be (result)
   }
 }
