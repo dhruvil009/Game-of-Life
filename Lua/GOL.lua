5daccf3741234e75f62dc7eefaf8abd63c3ef17d
neeraj <nirajshukla10@gmail.com>
2020-08-24 11:51:03 -0400

    Update GameOfLife.lua

--- Lua/GOL.lua
+++ Lua/GOL.lua
@@ -1,96 +1,109 @@
-print("This is Game of Life")
-m, n = 10, 10
--- This is a counter for the number of generations to be produced. Here, it is 10
-generations = 100
-print("value of m " ,m)
-print("value of n ",n, "\n")
+local GameOfLife = {}
 
-first_generation  = {} -- create the matrix
+function GameOfLife : createFirstGeneration(m, n)
 
-for i=1,m do
-    first_generation[i] = {}     -- create a new row
+   first_generation  = {} -- create the matrix
+   for i=1,m do
+      first_generation[i] = {}     -- create a new row
 
       for j=1,n do
         first_generation[i][j] = math.random(0,1)
       end
+   end
+
+   return first_generation
 
 end
 
-function printMatrix(matrix)
+function GameOfLife : printMatrix(matrix, m, n)
 
-    for i=1,m do
+   for i=1,m do
 
-        for j=1,n do
-            if(matrix[i][j] == 0) then
-                io.write(".")
-            else
-                io.write("*")
-            end
-        end
-        print("\n")
+      for j=1,n do
+         if(matrix[i][j] == 0) then
+            io.write(".")
+         else
+            io.write("*")
+         end
+      end
+      print("\n")
 
-    end
+   end
 
 end
 
--- Printing the first randomly produced value generation
-print("First generation : ")
-printMatrix(first_generation)
-
-function nextGeneration()
+function GameOfLife : createNextGeneration(first_generation, m, n)
     
     -- Initialising next generation
-    local next_generation = {}
-    for i=1,m do
-        next_generation[i] = {}    
+   local next_generation = {}
+   for i=1,m do
+      next_generation[i] = {}    
     
-          for j=1,n do
-            next_generation[i][j] = 0
-          end
+      for j=1,n do
+         next_generation[i][j] = 0
+      end
     
-    end
+   end
 
     -- Logic for producing next generation
-    for i = 1,m do
-        for j = 1,n do
-            local count = 0
-            for p = i-1,i+1 do
-                for q = j-1,j+1 do
-                    if p > 0 and p <= m and q > 0 and q <= n then
-                        count = count + tonumber(first_generation[p][q])
-                    end                  
-                end
+   for i = 1,m do
+      for j = 1,n do
+         local count = 0
+         for p = i-1,i+1 do
+            for q = j-1,j+1 do
+               if p > 0 and p <= m and q > 0 and q <= n then
+                  count = count + tonumber(first_generation[p][q])
+               end                  
             end
+         end
 
-            count = count - tonumber(first_generation[i][j])
+         count = count - tonumber(first_generation[i][j])
 
-            if count == 2 or count == 3 then
-                if first_generation[i][j] == 1 then
-                    next_generation[i][j] = 1
-                else
-                    if count == 2 then
-                        next_generation[i][j] = 0
-                    else
-                        next_generation[i][j] = 1
-                    end
-
-                end
+         if count == 2 or count == 3 then
+            if first_generation[i][j] == 1 then
+               next_generation[i][j] = 1
             else
-                next_generation[i][j] = 0
+               if count == 2 then
+                  next_generation[i][j] = 0
+               else
+                  next_generation[i][j] = 1
+               end
+
             end
-        end
-    end
+         else
+            next_generation[i][j] = 0
+         end
+      end
+   end
 
-    -- print(number_of_gen)
-    printMatrix(next_generation)
-    
-    first_generation = next_generation
-    generations = generations - 1
+   return next_generation
 
 end
 
-while( generations > 0)
-do 
-    nextGeneration()
+function GameOfLife : playGOL()
+
+   m,n  = 5,5
+   generation = 5
+
+   first_generation = GameOfLife : createFirstGeneration(m,n)
+
+   print("------ Let's play Game of Life ------  " , '\n')
+   GameOfLife : printMatrix(first_generation, m, n)
+
+   while( generation > 0)
+   do
+      next_generation = GameOfLife : createNextGeneration(first_generation, m, n)
+      GameOfLife : printMatrix(next_generation, m, n)
+      first_generation = next_generation
+      generation = generation - 1
+   end
+
 end
+
+GameOfLife : playGOL()
+
+return GameOfLife
+
+ 
+
 
